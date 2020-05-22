2ca87f4eab6602caa3f42f001e453c0439164696
Ankit Mahajan <ankit_mahajan@intuit.com>
2020-05-22 21:54:57 +0530

    Added my project

---
+++ src/test/java/com/ankit/consoletest/consoletestapp/ConsoleTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.consoletest.consoletestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
